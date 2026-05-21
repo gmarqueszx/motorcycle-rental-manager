258b84bfd8c34c1b1ef64b6d13ebfc836d770bb8
João Gabriel Marques <bielpesso@gmail.com>
2026-05-20 23:33:10 -0300

    first commit

---
+++ src/main/java/space/gmarqueszx/motorcycle_rental/MotorcycleRentalApplication.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.motorcycle_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorcycleRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorcycleRentalApplication.class, args);
+	}
+
+}
